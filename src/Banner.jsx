002ae7cbca5700096121f9e15a647a9e72aebb99
Rounabh Sahu <godemode12@gmail.com>
2023-06-17 12:20:14 +0530

    updated input field

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Banner = () => {
-
+    const [text, setText] = useState('');
     return (
         <div className='banner'>
             <div className='navbar'>
@@ -18,8 +18,8 @@ const Banner = () => {
                 <div className='banner-main-text'>Bilgi Teknolojilerinde 23 Yıllık Tecrübe</div>
                 <div className='banner-sub-text'>Müşterilerimizin yüksek kalite seviyelerini koruyabilmeleri için farklı sektörlerde tecrübe kazanmış uzman kadrolarımızla Proje Yönetimi, İş Analizi ve Test Yönetimi hizmetleri sunmaktayız.</div>
                 <div className='banner-mail-input-button-container'>
-                    <input contentEditable='true' className='banner-mail-input' type='email' placeholder='Mail bültenimize kayıt ol'/>
-                    <div className='banner-mail-input-button'>Kayıt Ol</div>
+                    <input contentEditable='true' className='banner-mail-input' type='email' placeholder='Mail bültenimize kayıt ol' value={text} onChange={(e)=>setText(e.target.value)}/>
+                    <button className='banner-mail-input-button'>Kayıt Ol</button>
                     {/*<div className='banner-mail-input-button-text'></div>*/}
                 </div>
             </div>
